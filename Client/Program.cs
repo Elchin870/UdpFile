fba25f2a0f7a4382e305d816f66886146e6d9311
Elchin <Elchin@DESKTOP-HMRVE90>
2024-10-20 11:51:35 +0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ip = IPAddress.Loopback;
+            var port = 27001;
+            var ep = new IPEndPoint(ip, port);
+            var udpClient = new UdpClient();
+            Console.WriteLine("Client");
+
+            try
+            {
+                Console.Write("Enter file path: ");
+                var filePath = Console.ReadLine()!;
+                var fileName = Path.GetFileName(filePath);
+                var fileNameBytes = Encoding.UTF8.GetBytes(fileName + "\n");
+                udpClient.Send(fileNameBytes, fileNameBytes.Length, ep); 
+
+                using (var fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read))
+                {
+                    var bytes = new byte[1024];
+                    int len;
+                    while ((len = fs.Read(bytes, 0, bytes.Length)) > 0)
+                    {
+                        udpClient.Send(bytes, len, ep);
+                    }
+                }
+
+                udpClient.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server_Client_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ip = IPAddress.Loopback;
+            var port = 27001;
+            var ep = new IPEndPoint(ip, port);
+            var udpServer = new UdpClient(ep);
+            Console.WriteLine("Server");
+
+            try
+            {
+                while (true)
+                {
+                    var clientEp = new IPEndPoint(IPAddress.Any, 0);
+                    var fileNameData = udpServer.Receive(ref clientEp);
+                    var fileName = Encoding.UTF8.GetString(fileNameData).Trim();
+
+                    Console.WriteLine($"{clientEp} is sending file to server");
+
+                    var directoryPath = Path.Combine(Environment.CurrentDirectory, clientEp.Address.ToString());
+                    if (!Directory.Exists(directoryPath))
+                        Directory.CreateDirectory(directoryPath);
+
+                    var filePath = Path.Combine(directoryPath, fileName);
+
+
+                    using (var fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        udpServer.Client.ReceiveTimeout = 5000; 
+                        try
+                        {
+                            while (true)
+                            {
+                                var fileData = udpServer.Receive(ref clientEp); 
+                                fs.Write(fileData, 0, fileData.Length);
+                            }
+                        }
+                        catch (SocketException ex) when (ex.SocketErrorCode == SocketError.TimedOut)
+                        {
+                            Console.WriteLine("File transfer completed.");
+                        }
+                    }
+
+                    Console.WriteLine("File received");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
